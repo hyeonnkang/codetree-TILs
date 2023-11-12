b6424c6c1aa5eac695b25ac2a6a868b9c0f63308
KangHyeon <hyeonkang@inha.edu>
2023-11-13 01:06:02 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231112/방향에 맞춰 최대로 움직이기/max-movements-with-direction.cpp
+++ 231112/방향에 맞춰 최대로 움직이기/max-movements-with-direction.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #define MAX_N 4
@@ -13,54 +14,48 @@ int ans = 0; // 이동할 수 있는 최대 횟수
 
 void getInput(); // 사용자로부터 입력을 받는다
 bool isOutOfRange(int row, int col); // 격자 범위를 벗어난 경우 true return
-void move(int dir, int row, int col, int cnt); // (row, col)에서 dir 방향으로 이동하며 ans를 업데이트한다
+void move(int dir, int row, int col, int cnt, int pre_num); // (row, col)에서 dir 방향으로 이동하며 ans를 업데이트한다
 
 int main() {
     getInput();
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            move(direction[i][j], i, j, 0);
-        }
-    }
+    move(direction[r-1][c-1], r-1, c-1, 0, board[r-1][c-1]);
 
     cout << ans;
 
     return 0;
 }
 
-void move(int dir, int row, int col, int cnt){
+void move(int dir, int row, int col, int cnt, int pre_num) {
     int next_row = row;
     int next_col = col;
-    for(int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++) {
         next_row += dx[dir];
         next_col += dy[dir];
 
-
-        // 격자 범위를 벗어나거나 현재 숫자보다 크지 않은 경우
-        if(isOutOfRange(next_row, next_col)){
+        if (isOutOfRange(next_row, next_col)) { // 격자 범위를 벗어난 경우
             ans = max(ans, cnt);
             break;
-        }else if(board[next_row][next_col] <= board[row][col]){
+        } else if (board[next_row][next_col] <= pre_num) { // 이전 숫자보다 크지 않은 경우 건너뛰기
             continue;
-        }else{
-            move(dir, next_row, next_col, cnt);
-            move(direction[next_row][next_col], next_row, next_col, cnt+1);
+        } else {
+            move(dir, next_row, next_col, cnt, pre_num); // 방향 그대로
+            move(direction[next_row][next_col], next_row, next_col, cnt + 1, board[row][col]); // 방향 전환
         }
     }
 }
 
-void getInput(){
+void getInput() {
     cin >> n;
 
-    for(int i = 0; i < n; i++){
-        for(int j= 0; j < n; j++){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> board[i][j];
         }
     }
 
-    for(int i = 0; i < n; i++){
-        for(int j= 0; j < n; j++){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> direction[i][j];
         }
     }
@@ -68,6 +63,6 @@ void getInput(){
     cin >> r >> c;
 }
 
-bool isOutOfRange(int row, int col){
+bool isOutOfRange(int row, int col) {
     return (row < 0 || row >= n || col < 0 || col >= n);
 }
