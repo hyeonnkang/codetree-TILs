ccd458b1b146c2b920fe598a9afd265eed8e67fe
KangHyeon <hyeonkang@inha.edu>
2023-11-13 20:43:32 +0900

    Title: n개의 점 중 m개 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231113/n개의 점 중 m개 고르기/choose-m-out-of-n-points.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MX 20
+#define X first
+#define Y second
+#define INF 987654321
+
+int n, m;
+pair<int, int> dot[MX];
+pair<int, int> selected_dot[MX];
+int ans = INF; // 거리가 가장 먼 두 점 사이의 거리의 최솟값
+
+void getInput();
+int calculateMinDist(); // 고른 점들의 거리의 최솟값을 반환
+void selectDot(int cur, int cnt);
+
+int main() {
+    getInput();
+    selectDot(0, 0);
+    cout << ans;
+    return 0;
+}
+
+int calculateMinDist(){   
+    int res = INF;
+    for(int i = 0; i < m; i++){
+        for(int j = i+1; j < m; j++){
+            int x = abs(selected_dot[i].X - selected_dot[j].X);
+            int y = abs(selected_dot[i].Y - selected_dot[j].Y);
+            res = min(res, x*x + y*y);
+        }
+    }
+    return res;
+}
+
+void selectDot(int cur, int cnt){
+    if(cnt == m){ // m 개를 모두 고른 경우
+        ans = min(ans, calculateMinDist());
+        return;
+    }
+    if(cur == n){ // n개의 점을 모두 탐색한 경우
+        return;
+    }
+
+    // 현재 점을 선택한 경우
+    selected_dot[cnt] = dot[cur];
+    selectDot(cur+1, cnt+1);
+
+    // 현재 점을 선택하지 않은 경우
+    selectDot(cur+1, cnt);
+}
+
+void getInput(){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> dot[i].X >> dot[i].Y;
+    }
+}
