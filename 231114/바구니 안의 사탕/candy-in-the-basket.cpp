da1c43ca3deaa04642834e90a90c8c53b8e1ef66
KangHyeon <hyeonkang@inha.edu>
2023-11-14 15:52:42 +0900

    Title: 바구니 안의 사탕, Time: 85ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 231114/바구니 안의 사탕/candy-in-the-basket.cpp
+++ 231114/바구니 안의 사탕/candy-in-the-basket.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 #define MAX_N 100000
 #define MAX_POS 1000000
 
 int n, k;
-pair<int, int> candy[MAX_N+1]; // 사탕 갯수, 바구니 좌표
-int candy_cnt[MAX_POS+1];
-int candy_sum[MAX_POS+1];
+pair<ll, ll> candy[MAX_N+1]; // 사탕 갯수, 바구니 좌표
+ll candy_cnt[MAX_POS+1];
+ll candy_sum[MAX_POS+1];
 
 int main() {
     cin >> n >> k;
 
-    int pos_max = 0;
+    ll pos_max = 0;
     for(int i = 1; i <= n; i++){
         cin >> candy[i].first >> candy[i].second;
         candy_cnt[candy[i].second] = candy[i].first;
@@ -23,7 +24,7 @@ int main() {
         candy_sum[i] += candy_sum[i-1] + candy_cnt[i];
     }
 
-    int ans = 0; // 구간 내의 사탕의 최대 갯수
+    ll ans = 0; // 구간 내의 사탕의 최대 갯수
     int mid = k + 1;
     for(; mid + k <= pos_max; mid++){
         ans = max(ans, candy_sum[mid+k] - candy_sum[mid-k] + candy_cnt[mid-k]);
