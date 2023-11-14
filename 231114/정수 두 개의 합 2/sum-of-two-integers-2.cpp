45c947e171d8d8e8385b451fbeec104b93d089ba
KangHyeon <hyeonkang@inha.edu>
2023-11-14 15:08:11 +0900

    Title: 정수 두 개의 합 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231114/정수 두 개의 합 2/sum-of-two-integers-2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MX 100000
+
+int n, k;
+int num[MX];
+
+int main() {
+    cin >> n >> k;
+    for(int i = 0; i < n; i++){
+        cin >> num[i];
+    }
+
+    sort(num, num+n);
+
+    int j = 1;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        while(j < n){
+            if(num[i] + num[j] > k) break;
+
+            ans++;
+            j++;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
