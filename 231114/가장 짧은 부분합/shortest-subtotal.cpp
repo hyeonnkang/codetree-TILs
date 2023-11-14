0460e056db805dcf0551f82196ad1a33c536da6f
KangHyeon <hyeonkang@inha.edu>
2023-11-14 14:20:31 +0900

    Title: 가장 짧은 부분합, Time: 34ms, Memory: 0MB, Status: Passed - CodeTree

--- 231114/가장 짧은 부분합/shortest-subtotal.cpp
+++ 231114/가장 짧은 부분합/shortest-subtotal.cpp
@@ -3,26 +3,30 @@ using namespace std;
 
 #define MX 100000
 
-int n, s;
 int num[MX];
-int ans = INT_MAX;
 
 int main() {
+    int n, s;
     cin >> n >> s;
-    for(int i = 0; i < n; i++){
+
+    for(int i = 0; i< n; i++){
         cin >> num[i];
     }
 
+    int min_size = INT_MAX;
+    int sum = 0;
+    int j = -1;
     for(int i = 0; i < n; i++){
-        int sum = 0;
-        int j = i;
-        for(; j < n; j++){
-            sum += num[j];
-            if(sum >= s) break;
+        while(sum < s && j+1 < n){
+            sum += num[j+1];
+            j++;
         }
-        if(sum >= s) ans = min(ans, j-i+1);
+        if(sum < s) break;
+        int cur_size = j - i + 1;
+        min_size = min(min_size, cur_size);
+        sum -= num[i];
     }
-    cout << (ans == INT_MAX ? -1 : ans);
-    
+    cout << (min_size == INT_MAX ? -1 : min_size);
+
     return 0;
 }
