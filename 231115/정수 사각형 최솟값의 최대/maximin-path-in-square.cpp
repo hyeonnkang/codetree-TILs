1ca2a7b36a7c7b8f6ca124d009387094be53065c
KangHyeon <hyeonkang@inha.edu>
2023-11-15 11:29:37 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231115/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MX 100
+
+int n;
+int mat[MX+1][MX+1];
+int dp[MX+1][MX+1];
+
+void setFirstRow(){
+    for(int j = 2; j <= n; j++){
+        dp[1][j] = min(dp[1][j-1], mat[1][j]);
+    }
+}
+
+void setFirstCol(){
+    for(int i = 2; i <= n; i++){
+        dp[i][1] = min(dp[i-1][1], mat[i][1]);
+    }
+}
+
+int main() {
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cin >> mat[i][j];
+        }
+    }
+
+    for(int i = 1; i <= n; i++){
+        fill(dp[i]+1, dp[i]+n, INT_MAX);
+    }
+
+    dp[1][1] = mat[1][1];
+    setFirstRow();
+    setFirstCol();
+
+    for(int i = 2; i <= n; i++){
+        for(int j = 2; j <= n; j++){
+            dp[i][j] = max(min(dp[i-1][j], mat[i][j]), min(dp[i][j-1], mat[i][j]));
+        }
+    }
+
+    cout << dp[n][n];
+
+
+    return 0;
+}
