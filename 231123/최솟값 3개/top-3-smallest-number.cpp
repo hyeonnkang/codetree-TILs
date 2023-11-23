309065bce4bcd9908c78d24f3b1513c2fe7768b6
강현 <hyeonkang@inha.edu>
2023-11-23 14:30:11 +0900

    Title: 최솟값 3개, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231123/최솟값 3개/top-3-smallest-number.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+set<int> tree_set;
+vector<int> answer;
+
+int getMulResult(){
+    auto it = tree_set.begin();
+    int k = 0;
+    int res = 1;
+    while(k < 3){
+        res *= (*it);
+        it++;
+        k++;
+    }
+    return res;
+}
+
+int main() {
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        tree_set.insert(x);
+
+        if((int)tree_set.size() < 3) 
+            answer.push_back(-1);
+        else 
+            answer.push_back(getMulResult());
+    }
+
+    for(auto it = answer.begin(); it != answer.end(); it++){
+        cout << *it << "\n";
+    }
+
+    return 0;
+}
