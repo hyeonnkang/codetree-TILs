bcda16ad1de8035b7a8123325ad79f874390219c
강현 <hyeonkang@inha.edu>
2023-11-27 22:32:41 +0900

    Title: 씨 오 더블유, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231127/씨 오 더블유/c-o-w.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MX 100000
+
+int n;
+string s;
+int L[MX]; // C의 갯수
+int R[MX]; // W의 갯수
+
+int main() {
+    cin >> n;
+    cin >> s;
+
+    if(s[0] == 'C') L[0] = 1;
+    for(int i = 1; i < n; i++){
+        L[i] = L[i-1];
+        if(s[i] == 'C') L[i]++;
+    }
+    if(s[n-1] == 'W') R[n-1] = 1;
+    for(int i = n-2; i >= 0; i--){
+        R[i] = R[i+1];
+        if(s[i] == 'W') R[i]++;
+    }
+
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == 'O') ans += L[i] * R[i];
+    }
+    cout << ans;
+
+    return 0;
+}
