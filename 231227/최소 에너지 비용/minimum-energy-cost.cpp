2e7ba765400377f851f0b575fec407fed5016edf
강현 <hyeonkang@inha.edu>
2023-12-27 05:26:25 +0900

    Title: 최소 에너지 비용, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231227/최소 에너지 비용/minimum-energy-cost.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define MX_N 100000
+
+int n;
+int move_cost[MX_N];
+int charge_cost[MX_N];
+int min_charge_cost[MX_N]; // min_charge_cost[i] : 0~i까지 중 가장 작은 에너지 충전 비용
+
+int main() {
+    // 입력받기
+    cin >> n;
+    for(int i = 0; i < n-1; i++){
+        cin >> move_cost[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> charge_cost[i];
+    }
+
+    // 각 위치에서 최소 충전 비용 구하기
+    min_charge_cost[0] = charge_cost[0];
+    for(int i = 1; i < n; i++){
+        min_charge_cost[i] = min(charge_cost[i], min_charge_cost[i-1]);
+    }
+
+    // 필요한 충전 총량 구하기
+    ll sum = 0;
+    for(int i = 0; i < n-1; i++){
+        sum += move_cost[i] * min_charge_cost[i];
+    }  
+    cout << sum;
+
+    return 0;
+}
