721468b262177c797ef41ea23d60e268cd69625b
강현 <hyeonkang@inha.edu>
2023-12-27 05:45:57 +0900

    Title: 균일하게 나누기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231227/균일하게 나누기 2/divide-evenly-2.cpp
+++ 231227/균일하게 나누기 2/divide-evenly-2.cpp
@@ -15,8 +15,8 @@ int main() {
     }
 
     // 모두 더해서 x, y 평균값 구하기
-    double avg_x = 0;
-    double avg_y = 0;
+    int avg_x = 0;
+    int avg_y = 0;
     for(int i = 0; i < n; i++){
         avg_x += pos[i].first;
         avg_y += pos[i].second;
@@ -24,21 +24,26 @@ int main() {
     avg_x = avg_x / n;
     avg_y = avg_y / n;
 
-    // 각 영역 점의 갯수 구하기
-    for(int i = 0; i < n; i++){
-        if(pos[i].first < avg_x){
-            if(pos[i].second < avg_y) cnt[3]++;
-            else cnt[2]++;
-        }else{
-            if(pos[i].second < avg_y) cnt[4]++;
-            else cnt[1]++;
-        }
-    }
-
-    // 최대 점의 갯수 구하기
     int ans = 0;
-    for(int i = 0; i < 4; i++){
-        ans = max(ans, cnt[i]);
+    for(int k = 0; k < 2; k++, avg_x++, avg_y++){
+        // 각 영역 점의 갯수 구하기
+        fill(cnt, cnt+4, 0);
+        for(int i = 0; i < n; i++){
+            if(pos[i].first < avg_x){
+                if(pos[i].second < avg_y) cnt[3]++;
+                else cnt[2]++;
+            }else{
+                if(pos[i].second < avg_y) cnt[4]++;
+                else cnt[1]++;
+            }
+        }
+
+        // 최대 점의 갯수 구하기
+        int mx_cnt = 0;
+        for(int i = 0; i < 4; i++){
+            mx_cnt = max(mx_cnt, cnt[i]);
+        }
+        ans = max(ans, mx_cnt);
     }
     cout << ans;
 
